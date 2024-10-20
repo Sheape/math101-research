6712114776bb4bd53be8147158ba805b3a01830a
Sheape <86521166+Sheape@users.noreply.github.com>
2024-10-20 09:45:43 +0800

    refactor: cleaner version of counting cases

--- preprocessing.r
+++ preprocessing.r
@@ -1,6 +1,7 @@
-library(readxl)
 library(dplyr)
-library(stringr)
+
+source("utils.r")
+source("labels.r")
 
 # Load the data
 # MAKE SURE TO CREATE A COPY OF THE FILES FIRST
@@ -8,150 +9,52 @@ typhoid <- "Typhoid Fever (2011 - 2018).xlsx"
 abd <- "Acute Bloody Diarrhea (2011 - 2018).xlsx"
 geodata <- read.csv("geodata/baguio_city_geodata.csv")
 
-import_year <- function(data, year) {
-    year_data <- read_excel(data, sheet = year)
-    return(year_data)
-}
 
 #### Preprocessing ####
-typhoid11 <- read_excel(typhoid, sheet = "2011")
-typhoid12 <- read_excel(typhoid, sheet = "2012")
-typhoid13 <- read_excel(typhoid, sheet = "2013")
-typhoid14 <- read_excel(typhoid, sheet = "2014")
-typhoid15 <- read_excel(typhoid, sheet = "2015")
-typhoid16 <- read_excel(typhoid, sheet = "2016")
-typhoid17 <- read_excel(typhoid, sheet = "2017")
-typhoid18 <- read_excel(typhoid, sheet = "2018")
+typhoid11 <- import_year(typhoid, "2011")
+typhoid12 <- import_year(typhoid, "2012")
+typhoid13 <- import_year(typhoid, "2013")
+typhoid14 <- import_year(typhoid, "2014")
+typhoid15 <- import_year(typhoid, "2015")
+typhoid16 <- import_year(typhoid, "2016")
+typhoid17 <- import_year(typhoid, "2017")
+typhoid18 <- import_year(typhoid, "2018")
 
-abd11 <- read_excel(abd, sheet = "2011")
-abd12 <- read_excel(abd, sheet = "2012")
-abd13 <- read_excel(abd, sheet = "2013")
-abd14 <- read_excel(abd, sheet = "2014")
-abd15 <- read_excel(abd, sheet = "2015")
-abd16 <- read_excel(abd, sheet = "2016")
-abd17 <- read_excel(abd, sheet = "2017")
-abd18 <- read_excel(abd, sheet = "2018")
+abd11 <- import_year(abd, "2011")
+abd12 <- import_year(abd, "2012")
+abd13 <- import_year(abd, "2013")
+abd14 <- import_year(abd, "2014")
+abd15 <- import_year(abd, "2015")
+abd16 <- import_year(abd, "2016")
+abd17 <- import_year(abd, "2017")
+abd18 <- import_year(abd, "2018")
 
 ## FIXME: Convert numeric into Date
-## typhoid11[, 3:4] <- as.Date(typhoid11[, 3:4])
-cleanup_data <- function(col, patterns, replacements) {
-    cleaned_col <- str_squish(col)
-    for (i in seq_along(patterns)) {
-        cleaned_col <- gsub(patterns[i], replacements[i], cleaned_col)
-    }
-    return(cleaned_col)
-}
+typhoid11[, 3] <- as.Date(typhoid11[, 3], origin = "1899-12-30")
+typhoid11[, 4] <- as.Date(typhoid11[, 4], origin = "1899-12-30")
 
-patterns <- toupper(c(
-    "^Sto Tomas Proper$",
-    "^Sto. Tomas Proper$",
-    "^Lower Bokawkan$",
-    "^Bakakeng Norte$",
-    "^Bakakeng$",
-    "^Dominican -Mirador Hill$",
-    "^Harrison-Claudio$",
-    "^Sta Escolastica Village$",
-    "^Happy Homes-Campo Sioco$",
-    "^Aurora Hill Proper$",
-    "^Dizon Subd$",
-    "^Quirino Hill Lower$",
-    "^Modernsite East$",
-    "^Quezon Hill Upper$",
-    "^Quezon Hill$",
-    "^St Joseph Village$",
-    "^Sanitary Camp North$",
-    "^Lourdes Subdivision Extention$",
-    "^Palma- Urbano$",
-    "^Dagsian Upper$",
-    "^Upper Market$",
-    "^Honeymoon- Holyghost$",
-    "^AZCKO$",
-    "^Quirino-Magsaysay,$",
-    "^Dominican Hil-Mirador Hill$",
-    "^Andres Bonifacio$",
-    "^Lourdes Extension$",
-    "^Ferguson$",
-    "^Roman Ayson Road$",
-    "^Engineer\'s Hill$",
-    "^General Emilio Aguinaldo$",
-    "^Quezon Hill, Middle$",
-    "^Modernsite, East$",
-    "^General Emilio Aguinaldo \\(Lower QM\\)$",
-    "^DPS Compound$",
-    "^Holyghost Extension$",
-    "^Ferdinand Happy Homes \\(Campo Sioco\\)$",
-    "^Burnham-Legarda-Kisad$",
-    "^Imelda Marcos$",
-    "^Loakan-Apugan$",
-    "^Bayan Park, West$",
-    "^Camp$",
-    "^Navy Base$",
-    "^Atab$",
-    "^Roman Ayson$"
-))
+cleaned_typhoid11 <- rename_barangays(typhoid11$Barangay, patterns, replacements)
 
-replacements <- toupper(c(
-    "Santo Tomas Proper",
-    "Santo Tomas Proper",
-    "Andres Bonifacio (Lower Bokawkan)",
-    "Bakakeng North",
-    "Bakakeng Central",
-    "Dominican Hill-Mirador",
-    "Harrison-Claudio Carantes",
-    "Santa Escolastica",
-    as.character(geodata[108, 2]),
-    as.character(geodata[53, 2]),
-    "Dizon Subdivision",
-    "Quirino Hill, Lower",
-    "Modern Site, East",
-    "Quezon Hill, Upper",
-    "Quezon Hill Proper",
-    "Saint Joseph Village",
-    "Sanitary Camp, North",
-    "Lourdes Subdivision Extension",
-    "Palma-Urbano (Cariño-Palma)",
-    "Dagsian, Upper",
-    "Market Subdivision, Upper",
-    "Honeymoon (Honeymoon-Holy Ghost)",
-    "Abanao-Zandueta-Kayong-Chugum-Otek (AZKCO)",
-    "General Emilio F. Aguinaldo (Quirino-Magsaysay, Lower)",
-    "Dominican Hill-Mirador",
-    "Andres Bonifacio (Lower Bokawkan)",
-    "Lourdes Subdivision Extension",
-    "Guisad Sorong",
-    "Campo Filipino",
-    "Engineers' Hill",
-    "General Emilio F. Aguinaldo (Quirino-Magsaysay, Lower)",
-    "Middle Quezon Hill Subdivision(Quezon Hill Middle)",
-    "Modern Site, East",
-    "General Emilio F. Aguinaldo (Quirino-Magsaysay, Lower)",
-    "DPS Area",
-    "Holy Ghost Extension",
-    "Ferdinand (Happy Homes-Campo Sioco)",
-    "Legarda-Burnham-Kisad",
-    "Imelda R. Marcos (La Salle)",
-    "Apugan-Loakan",
-    "Bayan Park West (Bayan Park)",
-    "City Camp Proper",
-    "Saint Joseph Village",
-    "Santo Tomas Proper",
-    "Campo Filipino"
-))
+df_cleaned_typhoid11 <- typhoid11[
+    cleaned_typhoid11 %in% toupper(geodata$ADM4_EN),
+]
 
-cleaned_typhoid11 <- cleanup_data(typhoid11$Barangay, patterns, replacements)
-print(unique(cleaned_typhoid11))
+df_cleaned_typhoid11 <- df_cleaned_typhoid11[!is.na(df_cleaned_typhoid11$Barangay),]
 
 not_typhoid11 <- typhoid11[
     !(cleaned_typhoid11 %in% toupper(geodata$ADM4_EN)),
 ]
-print(unique(not_typhoid11$Barangay))
 
 View(not_typhoid11)
-warnings()
-
-## print(toupper(geodata$ADM4_EN))
+View(df_cleaned_typhoid11)
 
 
+#### Typhoid 2011 ####
+column_names <- c("MorbidityWeek", short_brgy_names)
+init_values <- numeric(130)
+
+typhoid11_df_cases <- count_cases(column_names, init_values, short_brgy_names, df_cleaned_typhoid11)
+View(typhoid11_df_cases)
 
 B=typhoid11$Barangay
 SS=typhoid11$CASECLASS
---
+++ utils.r
@@ -0,0 +1,47 @@
+# Utility functions for preprocessing and training.
+library(stringr)
+library(openxlsx)
+
+source("labels.r")
+
+import_year <- function(data, year) {
+    year_data <- read.xlsx(data, sheet = year)
+    return(year_data)
+}
+
+rename_barangays <- function(col, patterns, replacements) {
+    cleaned_col <- str_squish(col)
+    for (i in seq_along(patterns)) {
+        cleaned_col <- gsub(patterns[i], replacements[i], cleaned_col)
+    }
+    return(cleaned_col)
+}
+
+count_cases <- function(cols, init_values, brgy_list, original_df) {
+    result_df <- data.frame(matrix(ncol = length(cols), nrow = 0))
+    colnames(result_df) <- cols
+
+    for (i in 1:52) {
+        week_df <- original_df[original_df$MorbidityWeek == i, ]
+
+        row <- init_values
+        row[1] <- i
+
+        for (j in seq_len(nrow(week_df))) {
+            if (week_df[j, "CASECLASS"] != "SUSPECT") {
+                print(week_df[j, "Barangay"])
+                index <- match(week_df[j, "Barangay"], toupper(brgy_list))
+                print(index)
+
+                if (!is.na(index)) {
+                    row[index + 1] <- row[index + 1] + 1
+                }
+            }
+        }
+
+        result_df <- rbind(result_df, row)
+        colnames(result_df) <- cols
+    }
+
+    return(result_df)
+}
